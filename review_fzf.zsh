b290b4b46d5d71bd72ad14f83dcf5353ef40fb2e
Damien Riquet <damien.riquet@stormshield.eu>
2015-11-13 16:17:41 +0100

    fzf file updated

--- review_fzf.zsh
+++ review_fzf.zsh
@@ -1,10 +1,15 @@
 # review - Generate reviewing templates
+
+BASE_DIR="/home/driquet/perso/reviewing"
+PYTHON="python"
+
 review() {
-	local out name
+	local out name config
+	config="$BASE_DIR/review.cfg"
 	while out=$(
-		python ~/perso/reviewing/template.py -l |
+		$PYTHON $BASE_DIR/template.py --config "$config" -l |
 		fzf --ansi --no-sort --tac --toggle-sort=ctrl-r); do
 			name=$(echo "$out" | cut -d':' -f2)
-			python ~/perso/reviewing/template.py --clipping "$name"
+			$PYTHON $BASE_DIR/template.py --config "$config" --clipping "$name"
 	done
 }
